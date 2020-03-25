2fa6165a7e8af36336c180f9370c6b7d0a12d1a5
Constantinoi <pescaalma@gmail.com>
2020-03-25 14:02:53 -0400

    Integração back com front Logout

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React , { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link , useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api'
@@ -12,6 +12,8 @@ export default function Profile(){
 
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
+
+    const history = useHistory();
 
     useEffect(()=>{
       api.get('profile',{
@@ -37,13 +39,18 @@ export default function Profile(){
         }
     }
 
+    function handleLogout(){
+        localStorage.clear();
+        history.push('/')
+    }
+
     return(
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero"/>
                 <span>Bem Vinda, {ongName}</span>
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button type="button" onClick={()=>handleLogout()}>
                     <FiPower size={18} color= "#E02041"/>
                 </button>
             </header>  
